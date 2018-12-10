253c5946cc49db16b2dcd37ad5fbcde6973b6efa
Rees Varney <reesvarney02@gmail.com>
2018-12-10 21:58:35 +0000

    Add files via upload

--- node_server.js
+++ node_server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 var app = express();
-port = process.env.PORT || 80
+port = process.env.PORT || 8080;
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
