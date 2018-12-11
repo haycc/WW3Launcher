b6af196fd2ff663c288130163e4f4f311a7a98d0
Rees Varney <reesvarney02@gmail.com>
2018-12-11 15:44:14 +0000

    Add files via upload

--- public/app.js
+++ public/app.js
@@ -4,15 +4,15 @@ var sortByProperty = function (property) {
     };
 };
 
-servers = serverlist[0]["response"]["servers"];
+var servers = serverlist[0].response.servers;
 
-function connecttoserver(ip){
-  window.location.href = "steam://connect/" + ip
-}
+function connecttoserver(ip) {
+  window.location.href = "steam://connect/" + String(ip);
+};
 
 function filltable(){
   servers.sort(sortByProperty('players'));
-  for(var i = 0; i < servers.length; i++) {
+  for (var i = 0; i < servers.length; i++) {
       var currentserver = servers[i];
       serverid = String(i);
       serverip = currentserver["addr"];
@@ -20,7 +20,8 @@ function filltable(){
       servermap = currentserver["map"];
       serverplayers = String(currentserver["players"]) + "/" + String(currentserver["max_players"]);
       if (currentserver["players"] > 0){
-        $("tbody").append('<tr><td>' + serverip + '</td><td>' + servername + '</td><td>' + servermap + '</td><td>' + serverplayers + '</td><td><button class="btn btn-primary" onclick="connecttoserver('+ serverip +')">Connect</button></td></tr>');
+        serverip2 = "'" + String(serverip) + "'";
+        $("tbody").append('<tr><td>' + serverip + '</td><td>' + servername + '</td><td>' + servermap + '</td><td>' + serverplayers + '</td><td><button class="btn btn-primary" onclick="connecttoserver('+ serverip2 +')">Connect</button></td></tr>');
     };
   };
 };
